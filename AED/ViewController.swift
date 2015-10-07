1693fcf5feb0ec92ac9a8c3678018df0ab7f598c
yummish <yang.yu2012@outlook.com>
2015-10-07 23:32:43 +0800

    Basic view AED page. The view can scroll now
    TODO:
    1. Label doesn't change line automatically when there is text overflow.
    2. The contentSize of scrollView is hard-coded.

--- AED/ViewController.swift
+++ AED/ViewController.swift
@@ -10,9 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Mark: Properties
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var previewImage: UIImageView!
+    @IBOutlet weak var floorPlanImage: UIImageView!
+    
+    @IBOutlet weak var buildingNameLabel: UILabel!
+    @IBOutlet weak var floorLabel: UILabel!
+    @IBOutlet weak var specificLocationLabel: UILabel!
+    @IBOutlet weak var directionToFindLabel: UILabel!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        buildingNameLabel.text = "万达广场"
+        floorLabel.text = "2F"
+        specificLocationLabel.text = "2楼扶梯左边"
+        directionToFindLabel.text = "detailed direction to find the AED"
+
+        directionToFindLabel.lineBreakMode = NSLineBreakMode.ByWordWrapping
+        directionToFindLabel.frame.size.width = 50
+        print(directionToFindLabel.frame)
         // Do any additional setup after loading the view, typically from a nib.
+        print(previewImage.frame)
+        print(previewImage)
+        print(previewImage.bounds)
+        print(floorPlanImage.frame)
+        
+        let height = previewImage.frame.origin.y + 320
+        print(height)
+        scrollView.contentSize.height = height
+        
     }
 
     override func didReceiveMemoryWarning() {
